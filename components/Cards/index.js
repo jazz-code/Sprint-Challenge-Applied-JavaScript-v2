ffe113e387bd8d33a474aabbefc9a0712792e749
jazz-code <ren.meta@gmail.com>
2019-07-12 12:16:20 -0400

    axios get logged and articles component layout

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,23 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`).then(data => {
+  console.log(data.data);
+});
+
+function Articles(obj) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  const imgDiv = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = ``;
+  const name = document.createElement("span");
+  name.textContent = `By `;
+}
