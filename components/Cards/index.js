45e60fe676f394dd0c89ab8e99ccf572f063d93c
jazz-code <ren.meta@gmail.com>
2019-07-12 14:41:08 -0400

    WIP trying different methods to create dynamic card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,16 +19,60 @@
 // Create a card for each of the articles and add the card to the DOM.
 const cards = document.querySelector(".cards-container");
 
+// const articles = [bootstrap, bootstrap2];
+
+// articles.forEach(article => {
+//     axios.get(`https://lambda-times-backend.herokuapp.com/${article}`).then(data => {
+//     const element = bootstrap(data.data)
+//     articles.appendChild(element)
+//     })
+// })
+
 axios.get(`https://lambda-times-backend.herokuapp.com/articles`).then(data => {
   //   console.log(data.data.articles.bootstrap[0].authorName);
   //   console.log(data.data.articles.javascript[0].headline);
-  console.log(data.data.articles.bootstrap[0]);
-  //   console.log(data.data.articles);
+  //   console.log(data.data.articles.bootstrap[0]);
+  console.log(data.data.articles);
 
   //   console.log("data", dataCards);
   const card = bootstrap(data.data);
   cards.appendChild(card);
 });
+
+// function getArticle(articleData){
+// axios.get(`https://lambda-times-backend.herokuapp.com/${articleData}`).then(data => {
+//   console.log(data);
+// });
+// }
+
+function article(obj) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  //   headline.textContent = obj.articles.bootstrap[0].headline;
+  headline.textContent = `obj.articles${obj.articles}`;
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  const imgDiv = document.createElement("div");
+  imgDiv.classList.add("img-container");
+
+  const img = document.createElement("img");
+  img.src = obj.articles.bootstrap[0].authorPhoto;
+
+  const name = document.createElement("span");
+  name.textContent = `By ${obj.articles.bootstrap[0].authorName}`;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  card.appendChild(imgDiv);
+  card.appendChild(img);
+  card.appendChild(name);
+
+  return card;
+}
 
 function bootstrap(obj) {
   const card = document.createElement("div");
@@ -48,6 +92,34 @@ function bootstrap(obj) {
 
   const name = document.createElement("span");
   name.textContent = `By ${obj.articles.bootstrap[0].authorName}`;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  card.appendChild(imgDiv);
+  card.appendChild(img);
+  card.appendChild(name);
+
+  return card;
+}
+
+function bootstrap2(obj) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  headline.textContent = obj.articles.bootstrap[1].headline;
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  const imgDiv = document.createElement("div");
+  imgDiv.classList.add("img-container");
+
+  const img = document.createElement("img");
+  img.src = obj.articles.bootstrap[1].authorPhoto;
+
+  const name = document.createElement("span");
+  name.textContent = `By ${obj.articles.bootstrap[1].authorName}`;
 
   card.appendChild(headline);
   card.appendChild(author);
